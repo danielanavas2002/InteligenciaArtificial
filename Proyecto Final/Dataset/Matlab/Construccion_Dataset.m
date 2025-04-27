69bd67100c1e8a86cd8fb3b27dd8a4e6cced0764
danielanavas2002 <132109593+danielanavas2002@users.noreply.github.com>
2025-04-26 18:42:52 -0600

    Construccion Dataset Proyecto

---
+++ Proyecto Final/Dataset/Matlab/Construccion_Dataset.m
@@ -0,0 +1,234 @@
+% UNIVERSIDAD DEL VALLE DE GUATEMALA
+% Inteligencia Artificial - CC3085
+%
+% Elda Daniela Navas Cinto - 211000
+% Proyecto Final - Construcción de Dataset
+
+% Limpiar espacio
+close all; clear; clc;
+
+% Definir parámetros
+Fs = 2000;                % Frecuencia de muestreo (Hz)
+tamano_ventana = 5;       % s 
+N = Fs * tamano_ventana;  % Número de Muetras
+f = Fs*(0:(N/2))/N;                      % Vector de frecuencias
+
+% Filtrado Señal
+f_low = 20;  % Frecuencia de corte inferior (20 Hz)
+f_high = 500;  % Frecuencia de corte superior (500 Hz)
+Wn = [f_low f_high] / (Fs / 2);  % Frecuencias normalizadas
+[b, a] = butter(4, Wn, 'bandpass');  % Diseñar el filtro Butterworth pasabanda Orden 4
+
+% Cargar datos (variable 'data')
+reposo = load('Reposo.mat', 'data');
+palma = load('Palma.mat', 'data');
+pinza = load('Pinza.mat', 'data');
+puno = load('Puño.mat', 'data');
+
+% Extraer y filtrar la variable 'data' de cada estructura
+reposo = filter(b, a, reposo.data);
+palma = filter(b, a, palma.data);
+pinza = filter(b, a, pinza.data);
+puno = filter(b, a, puno.data);
+puno = puno*2;
+
+%% DATASET REPOSO
+% Inicializar dataset para Reposo
+dataset_reposo = [];
+
+num_ventanas_rep = floor(length(reposo) / N); % Número de ventanas 
+
+% Recorrer cada 2 ventanas para extraer características
+for i = 1:num_ventanas_rep
+    ventana = reposo((i-1)*N+1:i*N); % Extraer la ventana de 5 segundos
+    
+    % Extraer características en dominio de tiempo
+    MAV = mean(abs(ventana));      % Mean Absolute Value
+    RMS = sqrt(mean(ventana.^2));  % Root Mean Square
+    V = var(ventana);              % Varianza
+    WL = sum(abs(diff(ventana)));  % Waveform Length
+    SSC = sum(diff(ventana) > 0);  % Slope Sign Changes
+    IEMG = sum(abs(ventana));      % Integral of EMG
+    LOGVAR = log(var(ventana));  % Log-variance
+    
+    % Transformada de Fourier FFT
+    Y = fft(ventana);           % Transformada de Fourier
+    Y = abs(Y/N);               % Magnitud normalizada
+    Y = Y(1:N/2+1);             % Solo el espectro positivo
+    Y(2:end-1) = 2*Y(2:end-1);  % Escala adecuada
+
+    % Extraer características en dominio de frecuencia usando FFT
+    PF = f(find(Y == max(Y), 1));  % Peak Frequency
+    TP = sum(Y.^2);                % Total Power
+    SE = -sum((Y ./ sum(Y)) .* log(Y ./ sum(Y)));                                         % Spectral Entropy
+    FR = sum(Y(f >= 50 & f <= 150)) / sum(Y(f >= 0 & f <= 500));                          % Frequency Ratio (50-150 Hz)
+    BW = f(find(Y >= 0.5 * max(Y), 1, 'first')) - f(find(Y >= 0.5 * max(Y), 1, 'last'));  % Bandwidth
+    P2P = max(Y) - min(Y);         % Peak-to-Peak
+    PSD = sum(Y.^2);               % Power Spectral Density (potencia total)
+    MOV = "reposo";
+
+    % Guardar caracterísitcas en el dataset
+    dataset_reposo = [dataset_reposo; MAV, RMS, V, WL, SSC, IEMG, LOGVAR, PF, TP, SE, FR, BW, PSD, P2P, MOV];
+end
+
+% Guardar el dataset en un archivo .mat
+save('dataset_reposo.mat', 'dataset_reposo');
+
+%% DATASET PALMA EXTENDIDA
+% Inicializar dataset para Palma
+dataset_palma = [];
+
+num_ventanas = floor(length(palma) / N); % Número de ventanas 
+
+% Recorrer cada 2 ventanas para extraer características
+for i = 1:2:num_ventanas
+    ventana = palma((i-1)*N+1:i*N); % Extraer la ventana de 5 segundos
+    
+    % Extraer características en dominio de tiempo
+    MAV = mean(abs(ventana));      % Mean Absolute Value
+    RMS = sqrt(mean(ventana.^2));  % Root Mean Square
+    V = var(ventana);              % Varianza
+    WL = sum(abs(diff(ventana)));  % Waveform Length
+    SSC = sum(diff(ventana) > 0);  % Slope Sign Changes
+    IEMG = sum(abs(ventana));      % Integral of EMG
+    LOGVAR = log(var(ventana));  % Log-variance
+    
+    % Transformada de Fourier FFT
+    Y = fft(ventana);           % Transformada de Fourier
+    Y = abs(Y/N);               % Magnitud normalizada
+    Y = Y(1:N/2+1);             % Solo el espectro positivo
+    Y(2:end-1) = 2*Y(2:end-1);  % Escala adecuada
+
+    % Extraer características en dominio de frecuencia usando FFT
+    PF = f(find(Y == max(Y), 1));  % Peak Frequency
+    TP = sum(Y.^2);                % Total Power
+    SE = -sum((Y ./ sum(Y)) .* log(Y ./ sum(Y)));                                         % Spectral Entropy
+    FR = sum(Y(f >= 50 & f <= 150)) / sum(Y(f >= 0 & f <= 500));                          % Frequency Ratio (50-150 Hz)
+    BW = f(find(Y >= 0.5 * max(Y), 1, 'first')) - f(find(Y >= 0.5 * max(Y), 1, 'last'));  % Bandwidth
+    P2P = max(Y) - min(Y);         % Peak-to-Peak
+    PSD = sum(Y.^2);               % Power Spectral Density (potencia total)
+    MOV = "palma";
+
+    % Guardar caracterísitcas en el dataset
+    dataset_palma = [dataset_palma; MAV, RMS, V, WL, SSC, IEMG, LOGVAR, PF, TP, SE, FR, BW, PSD, P2P, MOV];
+end
+
+% Guardar el dataset en un archivo .mat
+save('dataset_palma.mat', 'dataset_palma');
+
+%% DATASET PINZA
+% Inicializar dataset para Pinza
+dataset_pinza = [];
+
+% Recorrer cada 2 ventanas para extraer características
+for i = 1:2:num_ventanas
+    ventana = pinza((i-1)*N+1:i*N); % Extraer la ventana de 5 segundos
+    
+    % Extraer características en dominio de tiempo
+    MAV = mean(abs(ventana));      % Mean Absolute Value
+    RMS = sqrt(mean(ventana.^2));  % Root Mean Square
+    V = var(ventana);              % Varianza
+    WL = sum(abs(diff(ventana)));  % Waveform Length
+    SSC = sum(diff(ventana) > 0);  % Slope Sign Changes
+    IEMG = sum(abs(ventana));      % Integral of EMG
+    LOGVAR = log(var(ventana));  % Log-variance
+    
+    % Transformada de Fourier FFT
+    Y = fft(ventana);           % Transformada de Fourier
+    Y = abs(Y/N);               % Magnitud normalizada
+    Y = Y(1:N/2+1);             % Solo el espectro positivo
+    Y(2:end-1) = 2*Y(2:end-1);  % Escala adecuada
+
+    % Extraer características en dominio de frecuencia usando FFT
+    PF = f(find(Y == max(Y), 1));  % Peak Frequency
+    TP = sum(Y.^2);                % Total Power
+    SE = -sum((Y ./ sum(Y)) .* log(Y ./ sum(Y)));                                         % Spectral Entropy
+    FR = sum(Y(f >= 50 & f <= 150)) / sum(Y(f >= 0 & f <= 500));                          % Frequency Ratio (50-150 Hz)
+    BW = f(find(Y >= 0.5 * max(Y), 1, 'first')) - f(find(Y >= 0.5 * max(Y), 1, 'last'));  % Bandwidth
+    P2P = max(Y) - min(Y);         % Peak-to-Peak
+    PSD = sum(Y.^2);               % Power Spectral Density (potencia total)
+    MOV = "pinza";
+
+    % Guardar caracterísitcas en el dataset
+    dataset_pinza = [dataset_pinza; MAV, RMS, V, WL, SSC, IEMG, LOGVAR, PF, TP, SE, FR, BW, PSD, P2P, MOV];
+end
+
+% Guardar el dataset en un archivo .mat
+save('dataset_pinza.mat', 'dataset_pinza');
+
+%% DATA SET PUÑO CERRADO
+% Inicializar dataset para Puño
+dataset_puno = [];
+
+% Recorrer cada 2 ventanas para extraer características
+for i = 1:2:num_ventanas
+    ventana = puno((i-1)*N+1:i*N); % Extraer la ventana de 5 segundos
+    
+    % Extraer características en dominio de tiempo
+    MAV = mean(abs(ventana));      % Mean Absolute Value
+    RMS = sqrt(mean(ventana.^2));  % Root Mean Square
+    V = var(ventana);              % Varianza
+    WL = sum(abs(diff(ventana)));  % Waveform Length
+    SSC = sum(diff(ventana) > 0);  % Slope Sign Changes
+    IEMG = sum(abs(ventana));      % Integral of EMG
+    LOGVAR = log(var(ventana));  % Log-variance
+    
+    % Transformada de Fourier FFT
+    Y = fft(ventana);           % Transformada de Fourier
+    Y = abs(Y/N);               % Magnitud normalizada
+    Y = Y(1:N/2+1);             % Solo el espectro positivo
+    Y(2:end-1) = 2*Y(2:end-1);  % Escala adecuada
+
+    % Extraer características en dominio de frecuencia usando FFT
+    PF = f(find(Y == max(Y), 1));  % Peak Frequency
+    TP = sum(Y.^2);                % Total Power
+    SE = -sum((Y ./ sum(Y)) .* log(Y ./ sum(Y)));                                         % Spectral Entropy
+    FR = sum(Y(f >= 50 & f <= 150)) / sum(Y(f >= 0 & f <= 500));                          % Frequency Ratio (50-150 Hz)
+    BW = f(find(Y >= 0.5 * max(Y), 1, 'first')) - f(find(Y >= 0.5 * max(Y), 1, 'last'));  % Bandwidth
+    P2P = max(Y) - min(Y);         % Peak-to-Peak
+    PSD = sum(Y.^2);               % Power Spectral Density (potencia total)
+    MOV = "puno";
+
+    % Guardar caracterísitcas en el dataset
+    dataset_puno = [dataset_puno; MAV, RMS, V, WL, SSC, IEMG, LOGVAR, PF, TP, SE, FR, BW, PSD, P2P, MOV];
+end
+
+% Guardar el dataset en un archivo .mat
+save('dataset_puno.mat', 'dataset_puno');
+
+%% CONCATENAR TODAS LAS BASES
+close all; clear; clc;
+
+% Cargar los datasets
+load('dataset_reposo.mat'); % Cargar dataset_reposo
+load('dataset_palma.mat');  % Cargar dataset_palma
+load('dataset_pinza.mat');  % Cargar dataset_pinza
+load('dataset_puno.mat');   % Cargar dataset_puno
+
+% Concatenar los datos en un solo dataset
+dataset_completo = [dataset_reposo; dataset_palma; dataset_pinza; dataset_puno]; 
+
+% Crear el encabezado
+encabezado = {'MAV', 'RMS', 'V', 'WL', 'SSC', 'IEMG', 'LOGVAR', 'PF', 'TP', 'SE', 'FR', 'BW', 'PSD', 'P2P', 'MOV'};
+
+% Escribir el dataset combinado en un archivo CSV
+csvwrite_with_headers('dataset_completo.csv', dataset_completo, encabezado);
+
+% Función para escribir CSV con encabezado
+function csvwrite_with_headers(filename, data, headers)
+    % Abrir el archivo para escribir
+    fid = fopen(filename, 'w');
+    
+    % Escribir los encabezados
+    fprintf(fid, '%s,', headers{1:end-1});
+    fprintf(fid, '%s\n', headers{end});
+    
+    % Escribir los datos
+    for i = 1:size(data, 1)
+        fprintf(fid, '%f,', data(i, 1:end-1));
+        fprintf(fid, '%s\n', data{i, end});
+    end
+    
+    % Cerrar el archivo
+    fclose(fid);
+end
---
+++ Proyecto Final/Dataset/Matlab/Graficas_Data.m
@@ -0,0 +1,140 @@
+% UNIVERSIDAD DEL VALLE DE GUATEMALA
+% Inteligencia Artificial - CC3085
+%
+% Elda Daniela Navas Cinto - 211000
+% Proyecto Final - Elaboración de Gráficas
+
+% Limpiar espacio
+close all; clear; clc;
+
+% Definir parámetros
+Fs = 2000;                % Frecuencia de muestreo (Hz)
+tamano_ventana = 5;       % s 
+N = Fs * tamano_ventana;  % Número de Muetras
+
+% Filtrado Señal
+f_low = 20;  % Frecuencia de corte inferior (20 Hz)
+f_high = 500;  % Frecuencia de corte superior (500 Hz)
+Wn = [f_low f_high] / (Fs / 2);  % Frecuencias normalizadas
+[b, a] = butter(4, Wn, 'bandpass');  % Diseñar el filtro Butterworth pasabanda Orden 4
+
+% Cargar datos (variable 'data')
+reposo = load('Reposo.mat', 'data');
+palma = load('Palma.mat', 'data');
+pinza = load('Pinza.mat', 'data');
+puno = load('Puño.mat', 'data');
+
+% Extraer la variable 'data' de cada estructura
+reposo = reposo.data;
+palma = palma.data;
+pinza = pinza.data;
+puno = puno.data;
+
+% Definir inicio y fin del primer movimiento
+inicio_movimiento = ( Fs * tamano_ventana * 3) + 1;                  
+fin_movimiento = inicio_movimiento + N - 1; 
+
+% Cortar segmentos de interés
+reposo_segmento = filter(b, a, reposo(inicio_movimiento:fin_movimiento));
+palma_segmento = filter(b, a, palma(inicio_movimiento:fin_movimiento));
+pinza_segmento = filter(b, a, pinza(inicio_movimiento:fin_movimiento));
+puno_segmento = filter(b, a, puno(inicio_movimiento:fin_movimiento));
+
+
+% Crear vector de tiempo de 0 a 5 segundos
+t = linspace(0,5,N);
+
+% Graficar Señal
+figure(1);
+
+subplot(2,2,1);
+plot(t, reposo_segmento, 'Color', [0 0.4470 0.7410]); 
+title('Reposo', 'FontSize', 14);
+xlabel('Tiempo (s)');
+ylabel('Amplitud (mV)');
+ylim([-1 1]);
+
+subplot(2,2,2);
+plot(t, palma_segmento, 'Color', [0.8500 0.3250 0.0980]); 
+title('Palma Extendida', 'FontSize', 14);
+xlabel('Tiempo (s)');
+ylabel('Amplitud (mV)');
+ylim([-1 1]);
+
+subplot(2,2,3);
+plot(t, pinza_segmento, 'Color', [0.4660 0.6740 0.1880]); 
+title('Pinza', 'FontSize', 14);
+xlabel('Tiempo (s)');
+ylabel('Amplitud (mV)');
+ylim([-1 1]);
+
+subplot(2,2,4);
+plot(t, puno_segmento, 'Color', [0.4940 0.1840 0.5560]); 
+title('Puño Cerrado', 'FontSize', 14);
+xlabel('Tiempo (s)');
+ylabel('Amplitud (mV)');
+ylim([-1 1]);
+
+sgtitle('Patrones Musculares', 'FontSize', 28, 'FontWeight', 'bold');
+
+%% TRANSFORMADA DE FOURIER
+f = Fs*(0:(N/2))/N; % Vector de frecuencias
+
+% FFT
+Y_reposo = fft(reposo_segmento);
+Y_reposo = abs(Y_reposo/N);
+Y_reposo = Y_reposo(1:N/2+1);
+Y_reposo(2:end-1) = 2*Y_reposo(2:end-1);
+
+Y_palma = fft(palma_segmento);
+Y_palma = abs(Y_palma/N);
+Y_palma = Y_palma(1:N/2+1);
+Y_palma(2:end-1) = 2*Y_palma(2:end-1);
+
+Y_pinza = fft(pinza_segmento);
+Y_pinza = abs(Y_pinza/N);
+Y_pinza = Y_pinza(1:N/2+1);
+Y_pinza(2:end-1) = 2*Y_pinza(2:end-1);
+
+Y_puno = fft(puno_segmento);
+Y_puno = abs(Y_puno/N);
+Y_puno = Y_puno(1:N/2+1);
+Y_puno(2:end-1) = 2*Y_puno(2:end-1);
+
+% Gráficas con stem
+figure(2);
+
+subplot(2,2,1);
+stem(f, Y_reposo, 'Color', [0 0.4470 0.7410]);
+title('Reposo', 'FontSize', 14);
+xlabel('Frecuencia (Hz)');
+ylabel('|P(f)|');
+xlim([0 1000]);
+ylim([0 0.016]);
+
+subplot(2,2,2);
+stem(f, Y_palma, 'Color', [0.8500 0.3250 0.0980]);
+title('Palma Extendida', 'FontSize', 14);
+xlabel('Frecuencia (Hz)');
+ylabel('|P(f)|');
+xlim([0 1000]);
+ylim([0 0.016]);
+
+subplot(2,2,3);
+stem(f, Y_pinza, 'Color', [0.4660 0.6740 0.1880]);
+title('Pinza', 'FontSize', 14);
+xlabel('Frecuencia (Hz)');
+ylabel('|P(f)|');
+xlim([0 1000]);
+ylim([0 0.016]);
+
+subplot(2,2,4);
+stem(f, Y_puno, 'Color', [0.4940 0.1840 0.5560]);
+title('Puño Cerrado', 'FontSize', 14);
+xlabel('Frecuencia (Hz)');
+ylabel('|P(f)|');
+xlim([0 1000]);
+ylim([0 0.016]);
+
+sgtitle('Espectro Unilateral de la Señal', 'FontSize', 28, 'FontWeight', 'bold');
+
